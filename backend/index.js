a236dff20b1e6562445f84c6ef04a9ce33309354
Vivek <mailvivekkv@gmail.com>
2023-07-02 14:11:12 +0530

    port

--- backend/index.js
+++ backend/index.js
@@ -60,7 +60,7 @@ app.use("/api/blogpcat", blogCatRoute);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
